74c92981a07ea555975bbc0b85678ed85ab9a8ba
Norvin <chnorvin@gmail.com>
2022-11-15 14:54:30 -0800

    Function Activity

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// function greet(user){
+// console.log(`Hello ${user}`)
+// }
+
+// greet("JOTARO");
+// let name = function () {
+
+// }
+// const greet = function (user){
+//     console.log(`Hello ${user}`)
+// }
+// greet("DIO")
+// const greet = (user) =>
+// {
+//     console.log(`KONO ${user} DA WRRYYYYYYYYYYYY. MUDA MUDA`);
+// }
+// greet("DIO")
+function greet(user, time = "day")
+{
+    console.log(`Good ${time} ${user}`);
+}
+greet("JoJo")
+greet("JoJo", "night")
